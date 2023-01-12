e1b0d1b42be850aa31d5846dcaf7cd235209740a
h3nc4 <me@h3nc4.com>
2023-01-12 12:53:09 -0300

    Only commit

---
+++ src/config.sh
@@ -0,0 +1,15 @@
+# Variables
+# The date of the first commit, in the format YYYY-MM-DD
+START_DATE="2022-01-01"
+# The date of the last commit
+END_DATE="2023-01-01"
+# The commit message
+COMMIT_MESSAGE="Dont judge people by commits"
+# Some git repository path
+REPO_PATH="/path/to/MyProject"
+# Max number of commits per day
+MAX_COMMITS_PER_DAY=10
+# Min number of commits per day
+MIN_COMMITS_PER_DAY=0
+# Chance that there will be no commits on a day, ***IN PERCENT***
+NO_COMMITS_CHANCE=20
